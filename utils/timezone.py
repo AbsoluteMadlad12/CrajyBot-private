a3ffb4e1b6d72f0a2b29bf8b3d2ff0a7ce1e88ed
Anand Krishna <anandkrishna2312@gmail.com>
2020-12-14 20:43:17 +0400

    Beginning work on restructuring Metrics commands

--- utils/timezone.py
+++ utils/timezone.py
@@ -1,3 +1,23 @@
 import pytz
+from datetime import timedelta
 
 BOT_TZ = pytz.timezone("Asia/Dubai")
+
+def get_timedelta(arg: str) -> timedelta:
+    """Converts a string of time for eg: 5h -> into an equivalent timedelta object."""
+    time_, unit = "", ""
+    for i in arg:
+        if i.isdigit():
+            time_ += i
+        else:
+            unit += i
+    else:
+        time_ = int(time_)
+
+    if unit.lower().startswith("h"):
+        return timedelta(hours=time_)
+    elif unit.lower().startswith("d"):
+        return timedelta(days=time_)
+    else:
+        raise TypeError(f"Unsupported unit of time. Expecting `hours` or `days`, got {unit}")
+    
