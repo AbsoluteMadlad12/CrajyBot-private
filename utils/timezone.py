695ee0b92d253efd0173e4b807308ea9359d5d61
Anand Krishna <anandkrishna2312@gmail.com>
2020-12-17 12:36:46 +0400

    Returning more info with `get_timedelta`

--- utils/timezone.py
+++ utils/timezone.py
@@ -1,10 +1,13 @@
 import pytz
 from datetime import timedelta
+from collections import namedtuple
 
 BOT_TZ = pytz.timezone("Asia/Dubai")
 
-def get_timedelta(arg: str) -> timedelta:
-    """Converts a string of time for eg: 5h -> into an equivalent timedelta object."""
+Timedelta = namedtuple("Timedelta", "delta unit")
+
+def get_timedelta(arg: str) -> Timedelta[timedelta, str]:
+    """Converts a string of time for eg: 5h -> into an equivalent timedelta object. Returns a namedtuple with the timedelta and the unit as a string."""
     time_, unit = "", ""
     for i in arg:
         if i.isdigit():
@@ -15,9 +18,9 @@ def get_timedelta(arg: str) -> timedelta:
         time_ = int(time_)
 
     if unit.lower().startswith("h"):
-        return timedelta(hours=time_)
+        return Timedelta(delta=timedelta(hours=time_), unit="hours")
     elif unit.lower().startswith("d"):
-        return timedelta(days=time_)
+        return Timedelta(delta=timedelta(days=time_), unit="days")
     else:
         raise TypeError(f"Unsupported unit of time. Expecting `hours` or `days`, got {unit}")
     
